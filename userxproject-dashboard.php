32d1cbab94fa368e2b31deaf23deed14a2b3e7d8
Neil Engracia <neilengracia@icloud.com>
2014-12-15 10:24:03 -0800

    Create userxproject-dashboard.php

---
+++ userxproject-dashboard.php
@@ -0,0 +1,21 @@
+<?php
+
+require 'config.php';
+protected_area();
+
+
+?>
+
+<!doctype html>
+<html>
+    <body>
+        
+        <a href="index.php?logout"> Logout </a>
+        
+        <h1> DashBoard! </h1>
+        
+    </body>
+    
+    
+    
+</html>
